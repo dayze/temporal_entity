06bfdad7bec5c155e1c9d896c91e7d4ab09e1b2a
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2017-11-23 15:40:49 +0100

    Delete constructor without action of AbstractTemporalEntityWithActions.java.

--- src/com/company/model/AbstractTemporalEntityWithActions.java
+++ src/com/company/model/AbstractTemporalEntityWithActions.java
@@ -9,10 +9,6 @@ public class AbstractTemporalEntityWithActions extends AbstractTemporalEntity {
 
     private ArrayList<Action> actions;
 
-    public AbstractTemporalEntityWithActions(long startTime, long endTime) {
-        this(startTime, endTime, new ArrayList<>());
-    }
-
     public AbstractTemporalEntityWithActions(long startTime, long endTime, ArrayList<Action> actions) {
         super(startTime, endTime);
         this.actions = actions;
